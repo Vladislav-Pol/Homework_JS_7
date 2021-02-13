86320670af2375e0da6e745256a44e04fe655fd8
Vladislav-Pol <74258599+Vladislav-Pol@users.noreply.github.com>
2021-02-13 22:07:35 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,5 @@
 function Calculator(){
     this.state = false;
-    this.first_value = Number;
-    this.thecond_value = Number;
-    this.operation = String;
-    this.result = Number;
-
     this.on_off = function (){
         this.state = !this.state
     }
@@ -12,13 +7,13 @@ function Calculator(){
     this.calculate = function (){
         this.result = 0;
         this.first_value = Number(prompt("Введите первое число"));
-        if (!/\d+\.?\d*/.test(this.first_value))
+        if (isNaN(this.first_value))
             return alert(`Ошибка. Вы ввели не число.`)
         this.operation = prompt("Введите символ необходимой операции\n(+ - * /)");
         if (!/[+\-*\/]/.test(this.operation))
             return alert("Ошибка. Введенная оперция не распознана.")
         this.thecond_value = Number(prompt("Введите второе число"));
-        if (!/\d+\.?\d*/.test(this.thecond_value))
+        if (isNaN(this.thecond_value))
             return alert(`Ошибка. Вы ввели не число.`)
         switch (this.operation){
             case "+":
